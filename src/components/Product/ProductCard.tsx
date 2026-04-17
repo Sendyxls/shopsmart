c7ad6104296b5cc9a3c76b4f3765577387258d4e
Sendyxls <134251019+Sendyxls@users.noreply.github.com>
2026-04-17 15:02:21 +1000

    Update ProductCard.tsx

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -35,7 +35,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                 <h1 className="text-2xl font-bold text-gray-900 mb-2">{product.name}</h1>
 
                 <div className="flex justify-between items-center mb-4">
-                    <span className="text-3xl font-bold bg-brand-green">
+                    <span className="text-3xl font-bold">
                         {formatPrice(product.price)}
                     </span>
                     <div className="flex items-center bg-yellow-100 px-3 py-1 rounded-full">
